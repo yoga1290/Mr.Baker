590ff61d94b8b4f55faed7adef41632e93ab49c5
Youssef Gamil <yoga1290@gmail.com>
2012-01-04 20:31:21 +0200

    Adding the Employee form

---
+++ getEmployees.php
@@ -0,0 +1,20 @@
+<?php
+//Connect to the MySQL Database:
+mysql_connect('fdb2.awardspace.com','yoga1290_project','yoga1290');
+@mysql_select_db('yoga1290_project') or die( "Unable to select database");
+
+// SQL query:
+$query="SELECT Name,idEmployee FROM Employee WHERE Name like '%".$_GET['Employee_Name']."%'";
+$result=mysql_query($query) or die( mysql_error() );
+
+
+$n=mysql_numrows($result);
+
+// List Customers IDs & Names:
+for($i=0;$i<$n;$i++)
+	echo mysql_result($result,$i,"idProducts").":".mysql_result($result,$i,"Name")."\n";
+
+if($n<1)
+    echo "None :(";
+mysql_close();
+?>
