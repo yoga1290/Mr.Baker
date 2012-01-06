471a50625779808c10f14447b5c5173e6b11d620
Youssef Gamil <yoga1290@gmail.com>
2012-01-07 01:28:00 +0200

    field name fixed :)

--- getEmployees.php
+++ getEmployees.php
@@ -12,7 +12,7 @@ $n=mysql_numrows($result);
 
 // List Customers IDs & Names:
 for($i=0;$i<$n;$i++)
-	echo mysql_result($result,$i,"idProducts").":".mysql_result($result,$i,"Name")."\n";
+	echo mysql_result($result,$i,"idEmployee").":".mysql_result($result,$i,"Name")."\n";
 
 if($n<1)
     echo "None :(";
